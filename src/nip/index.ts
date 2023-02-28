6843eaf4148a920c67f8c0f838ae5c37511bea6a
Sebastian Fituch <sitemax@gmail.com>
2023-02-28 23:40:52 +0100

    refactor: use named function across lib

--- src/nip/index.ts
+++ src/nip/index.ts
@@ -2,7 +2,7 @@ import { calculateChecksum, NUMBERS_ONLY_REGEX } from "../utils";
 
 const WEIGHTS = [6, 5, 7, 2, 3, 4, 5, 6, 7];
 
-export const isValidNip = (nip: string): boolean => {
+export function isValidNip(nip: string): boolean {
   if (
     typeof nip !== "string" ||
     nip.length !== 10 ||
@@ -17,4 +17,4 @@ export const isValidNip = (nip: string): boolean => {
   const modulo = sum % 11;
 
   return modulo === checkDigit;
-};
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export const NUMBERS_ONLY_REGEX = /^\d+$/;
 
-export const calculateChecksum = (digits: number[], weights: number[]) => {
+export function calculateChecksum(digits: number[], weights: number[]): number {
   if (digits.length !== weights.length) {
     throw new Error(
       "The length of the weights is not equal to the length of the digits."
@@ -10,4 +10,4 @@ export const calculateChecksum = (digits: number[], weights: number[]) => {
   return digits
     .map((digit, index) => digit * weights[index])
     .reduce((sum, number) => sum + number, 0);
-};
+}
