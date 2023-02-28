271336d45097fd1979b9325ff0b4c4f6d50a3510
Sebastian Fituch <sitemax@gmail.com>
2023-02-28 23:40:52 +0100

    refactor: minor cosmetics in the PESEL function

--- src/pesel/index.ts
+++ src/pesel/index.ts
@@ -1,6 +1,35 @@
 import { calculateChecksum, NUMBERS_ONLY_REGEX } from "../utils";
 
 const WEIGHTS = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
+const MODULO_REMAINDER_TO_CENTURY = new Map([
+  [0, 1900],
+  [20, 2000],
+  [40, 2100],
+  [60, 2200],
+  [80, 1800],
+]);
+
+const isValidPeselDate = (pesel: string): boolean => {
+  const yearShortly = Number(pesel.slice(0, 2));
+  const increasedMonth = Number(pesel.slice(2, 4));
+  const day = Number(pesel.slice(4, 6));
+
+  // calculate the century and sum up the actual year
+  const month = increasedMonth % 20;
+  const monthIndex = month - 1;
+  const century = MODULO_REMAINDER_TO_CENTURY.get(increasedMonth - month)!;
+  const year = century + yearShortly;
+
+  const date = new Date(year, monthIndex, day);
+
+  // if any parameter overflows the defined bounds,
+  // it "carries over", so the parsed date must be compared
+  return (
+    date.getDate() === day &&
+    date.getMonth() === monthIndex &&
+    date.getFullYear() === year
+  );
+};
 
 export function isValidPesel(pesel: string): boolean {
   if (
@@ -21,36 +50,6 @@ export function isValidPesel(pesel: string): boolean {
   const modulo = sum % 10;
 
   return (modulo === 0 && checkDigit === 0) || 10 - modulo === checkDigit;
-}
-
-const MODULO_REMAINDER_TO_CENTURY: Record<number, number> = {
-  0: 1900,
-  20: 2000,
-  40: 2100,
-  60: 2200,
-  80: 1800,
-};
-
-function isValidPeselDate(pesel: string): boolean {
-  const yearShortly = Number(pesel.slice(0, 2));
-  const increasedMonth = Number(pesel.slice(2, 4));
-  const day = Number(pesel.slice(4, 6));
-
-  // calculate the century and sum up the actual year
-  const month = increasedMonth % 20;
-  const monthIndex = month - 1;
-  const century = MODULO_REMAINDER_TO_CENTURY[increasedMonth - month];
-  const year = century + yearShortly;
-
-  const date = new Date(year, monthIndex, day);
-
-  // if any parameter overflows the defined bounds,
-  // it "carries over", so the parsed date must be compared
-  return (
-    date.getDate() === day &&
-    date.getMonth() === monthIndex &&
-    date.getFullYear() === year
-  );
 }
 
 export const exportedForTesting = {
