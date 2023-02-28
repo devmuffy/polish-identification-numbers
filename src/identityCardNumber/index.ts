0ceb9ddb89e02dc03546f9eb5c2d51a82bba9026
Sebastian Fituch <sitemax@gmail.com>
2023-02-28 23:40:52 +0100

    refactor: switch back to Object for better compatibility

--- src/identityCardNumber/index.ts
+++ src/identityCardNumber/index.ts
@@ -1,34 +1,34 @@
 import { calculateChecksum } from "../utils";
 
 const FORMAT_REGEX = /^[a-zA-Z]{3}\d{6}$/;
-const LETTER_TO_VALUE = new Map([
-  ["A", 10],
-  ["B", 11],
-  ["C", 12],
-  ["D", 13],
-  ["E", 14],
-  ["F", 15],
-  ["G", 16],
-  ["H", 17],
-  ["I", 18],
-  ["J", 19],
-  ["K", 20],
-  ["L", 21],
-  ["M", 22],
-  ["N", 23],
-  ["O", 24],
-  ["P", 25],
-  ["Q", 26],
-  ["R", 27],
-  ["S", 28],
-  ["T", 29],
-  ["U", 30],
-  ["V", 31],
-  ["W", 32],
-  ["X", 33],
-  ["Y", 34],
-  ["Z", 35],
-]);
+const LETTER_TO_VALUE = {
+  A: 10,
+  B: 11,
+  C: 12,
+  D: 13,
+  E: 14,
+  F: 15,
+  G: 16,
+  H: 17,
+  I: 18,
+  J: 19,
+  K: 20,
+  L: 21,
+  M: 22,
+  N: 23,
+  O: 24,
+  P: 25,
+  Q: 26,
+  R: 27,
+  S: 28,
+  T: 29,
+  U: 30,
+  V: 31,
+  W: 32,
+  X: 33,
+  Y: 34,
+  Z: 35,
+};
 const WEIGHTS = [7, 3, 1, 9, 7, 3, 1, 7, 3];
 
 /**
@@ -46,7 +46,7 @@ export function isValidIdentityCardNumber(idNumber: string) {
 
   const chars = idNumber.toUpperCase().split("");
   const digits = [
-    ...chars.slice(0, 3).map((char) => LETTER_TO_VALUE.get(char)!),
+    ...chars.slice(0, 3).map((char) => LETTER_TO_VALUE[char]),
     ...chars.slice(3, 9).map((char) => parseInt(char, 10)),
   ];
   const sum = calculateChecksum(digits, WEIGHTS);
