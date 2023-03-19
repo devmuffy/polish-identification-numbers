e79e0575b8a2c69ac555237d6f58a04e56c986da
Sebastian Fituch <sitemax@gmail.com>
2023-03-19 01:55:51 +0100

    refactor(identityCardNumber, passportNumber): FIXME

---
+++ src/guards.ts
@@ -0,0 +1,30 @@
+export type UpperCaseCharacter =
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | "G"
+  | "H"
+  | "I"
+  | "J"
+  | "K"
+  | "L"
+  | "M"
+  | "N"
+  | "O"
+  | "P"
+  | "Q"
+  | "R"
+  | "S"
+  | "T"
+  | "U"
+  | "V"
+  | "W"
+  | "X"
+  | "Y"
+  | "Z";
+export function isUpperCaseCharacter(s: string): s is UpperCaseCharacter {
+  return /^[A-Z]$/.test(s);
+}
--- src/passportNumber/index.ts
+++ src/passportNumber/index.ts
@@ -1,34 +1,7 @@
-import { calculateChecksum, splitAt } from "../utils";
+import { LETTER_VALUES } from "../const";
+import { isUpperCaseCharacter } from "../guards";
+import { calculateChecksum, isNumeric, splitAt } from "../utils";
 
-const FORMAT_REGEX = /^[a-zA-Z]{2}\d{7}$/;
-const LETTER_TO_NUMBER: Record<string, number> = {
-  A: 10,
-  B: 11,
-  C: 12,
-  D: 13,
-  E: 14,
-  F: 15,
-  G: 16,
-  H: 17,
-  I: 18,
-  J: 19,
-  K: 20,
-  L: 21,
-  M: 22,
-  N: 23,
-  O: 24,
-  P: 25,
-  Q: 26,
-  R: 27,
-  S: 28,
-  T: 29,
-  U: 30,
-  V: 31,
-  W: 32,
-  X: 33,
-  Y: 34,
-  Z: 35,
-};
 const WEIGHTS = [7, 3, 9, 1, 7, 3, 1, 7, 3];
 
 /**
@@ -36,19 +9,23 @@ const WEIGHTS = [7, 3, 9, 1, 7, 3, 1, 7, 3];
  * isValidPassportNumber("CC7999486")
  */
 export function isValidPassportNumber(passportNumber: string): boolean {
+  if (typeof passportNumber !== "string" || passportNumber.length !== 9) {
+    return false;
+  }
+
+  const [letters, numerals] = splitAt(2, passportNumber.split(""));
+  const upperCaseLetters = letters.map((letter) => letter.toUpperCase());
+
   if (
-    typeof passportNumber !== "string" ||
-    passportNumber.length !== 9 ||
-    !FORMAT_REGEX.test(passportNumber)
+    !upperCaseLetters.every(isUpperCaseCharacter) ||
+    !numerals.every(isNumeric)
   ) {
     return false;
   }
 
-  const chars = passportNumber.toUpperCase().split("");
-  const [letters, digits] = splitAt(2, chars);
   const numbersAndDigits = [
-    ...letters.map((letter) => LETTER_TO_NUMBER[letter]),
-    ...digits.map(Number),
+    ...upperCaseLetters.map((letter) => LETTER_VALUES[letter]),
+    ...numerals.map(Number),
   ];
   const sum = calculateChecksum(numbersAndDigits, WEIGHTS);
   const modulo = sum % 10;
